f7d265555565fc80e5c5675819c3d06a6d39fdcd
Adam Azraei <adam.azrae@gmail.com>
2025-10-21 17:40:08 +0800

    test

---
+++ authenticate.php
@@ -0,0 +1,45 @@
+<?php
+require_once 'dbconfig.php';
+
+if ($SERVER["REQUEST_METHOD"] == "POST"){
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $sql = "SELECT id, email, password_hash, role FROM users WHERE email = ?";
+
+    if ($stmt = $conn->prepare($sql)){
+        $stmt->bind_param("s", $email);
+
+        if($stmt->execute()){
+            $stmt->store_result();
+            if($stmt->num_rows == 1){
+                $stmt->bind_result($id, $email, $hashed_password, $role);
+                $stmt->fetch();
+
+                if (password_verify($password, $hashed_password)){
+                    session_start();
+                    $_SESSION["loggedin"] = true;
+                    $_SESSION["id"] = $id;
+                    $_SESSION["email"] = $email;
+                    $_SESSION["role"] = $role;
+
+                    if($role == 'admin'){
+                        header("location: index-admin.html");
+                    } else{
+                        header("locaiton: index.html");
+                    }
+                    exit;
+                } 
+                else{
+                    $login_err = "Invalid username or password.";
+                    } 
+            }
+            else{
+                    $login_err = "Invalid username or password.";
+                }
+        }
+        $stmt->close();
+    }
+}
+$conn->close();
+?>
