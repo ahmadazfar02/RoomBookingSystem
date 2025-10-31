2aaac70ad4009e97078280bfcf4a7bc7d10bc552
Mohamed G. S Elmashharawi <wisamrubin123@gmail.com>
2025-10-31 18:13:40 +0800

    feat: forgot password step 1 complete

--- add_remember_me.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `users`
-ADD `remember_token` VARCHAR(255) NULL DEFAULT NULL AFTER `User_Type`,
-ADD `remember_token_expiry` DATETIME NULL DEFAULT NULL AFTER `remember_token`;
--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$_SESSION = array();
-session_destroy();
-setcookie('remember_me', '', time() - 3600, '/'); // Expire the cookie
-header("location: loginterface.html");
-exit;
-?>
